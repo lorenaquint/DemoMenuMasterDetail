881a8b184b598504bc94ea704934d86b8e7e3bc3
lorenaquint <lorenaquint@gmail.com>
2018-04-12 16:26:23 -0500

    Initial commit

---
+++ DemoMenuMasterDetail/DemoMenuMasterDetail/MasterPageItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoMenuMasterDetail
+{
+   public  class MasterPageItem
+    {
+        public string Titulo { get; set; }
+
+        public string Icono { get; set; }
+
+        public Type TipoPagina { get; set; }
+    }
+}
